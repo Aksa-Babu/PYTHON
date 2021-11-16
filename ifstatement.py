fcbcb54ae67ffa51c31b6f591e201b1e0ba70170
Aksa-Babu <aksababu212@gmail.com>
2021-11-16 10:43:53 +0530

    First Commit

---
+++ ifstatement.py
@@ -0,0 +1,4 @@
+a = 20 ; b = 20
+if ( a == b ):
+  print( “a and b are equal”)
+print(“If block ended”)
---
+++ test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   
+void main()
+{
+int a,b,c;
+a=10;
+b=3;
+c=a+b;
+printf("Result is%d:",c);
+}
